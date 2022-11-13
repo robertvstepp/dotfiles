f720a9d37f426195bac4257cb64f0ef12f9c8126
boB Stepp <robertvstepp@gmail.com>
2022-11-12 21:29:02 -0600

    Added i3 config

--- install.sh
+++ install.sh
@@ -12,3 +12,10 @@ ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 #######
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/dotfiles/X11" "$HOME/.config/X11"
+
+########
+#  i3  #
+########
+
+rm -rf "$HOME/.conf/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config/i3"
